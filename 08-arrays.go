91a7fcfb4e56b7a3aea35def257c397bddd7e5c3
Pelle Lundgren <pellburt@gmail.com>
2025-09-10 18:44:45 +0200

    feat(arrays): create an array

---
+++ 08-arrays.go
@@ -0,0 +1,11 @@
+// Arrays are fixed length sequences of elements of the same type.
+// Slices are more common.
+
+package main
+
+import "fmt"
+
+func main() {
+	var a [5]int
+	fmt.Println("emp:", a)
+}
