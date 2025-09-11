807675dbe91760e81b32c82b18acfc2043307759
Pelle Lundgren <pellburt@gmail.com>
2025-09-11 06:41:16 +0200

    feat(array): set value at index, get length and declare and initialize

--- 08-arrays.go
+++ 08-arrays.go
@@ -6,6 +6,20 @@ package main
 import "fmt"
 
 func main() {
+
+	// An array that hold 5 ints.
 	var a [5]int
 	fmt.Println("emp:", a)
+
+	// Set the value at index 4 to 100.
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+
+	// Get the length of the array.
+	fmt.Println("len:", len(a))
+
+	// Declare and initialize an array.
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
 }
