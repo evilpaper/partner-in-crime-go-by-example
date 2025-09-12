4013ccbfbe7e9444b7466e124de9a45745a39637
Pelle Lundgren <pellburt@gmail.com>
2025-09-12 06:54:51 +0200

    feat(slices): create a slice with make

--- 09-slices.go
+++ 09-slices.go
@@ -8,6 +8,13 @@ func main() {
 	// Slices are an important data type in Go, giving a more powerful interface to sequences than arrays.
 	
     // Slices are types by the elements they contain.
+	// Funky, similar syntax to arrays.
 	var s []string
 	fmt.Println("uninit:", s, s == nil, len(s) == 0)
+
+	// Create slice with non-zero length with make.
+	s = make([]string, 3)
+	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+	
+
 }
