96c25172a5085a8f5247b2a605d72311365dd524
Pelle Lundgren <pellburt@gmail.com>
2025-09-26 18:27:03 +0200

    feat(methods): define a method on a struct

---
+++ 20-methods.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// Go supports methods defined on struct types.
+
+type rect struct {
+	width, height int
+}
+
+// This area method has a receiver type of rect.
+func (r rect) area() int {
+	return r.width * r.height
+}
+
+func main() {
+	r := rect{width: 10, height: 5}
+	fmt.Println("Area:", r.area())
+}
