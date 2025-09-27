fb2867aaa8ebdc21ad04cece0974010c502bc8b1
Pelle Lundgren <pellburt@gmail.com>
2025-09-27 14:26:20 +0200

    feat(methods): receiver can also be values

--- 20-methods.go
+++ 20-methods.go
@@ -13,7 +13,14 @@ func (r rect) area() int {
 	return r.width * r.height
 }
 
+// This method has a receiver type of *rect.
+func (r *rect) perim() int {
+	return 2*r.width + 2*r.height
+}
+
 func main() {
 	r := rect{width: 10, height: 5}
+	
 	fmt.Println("Area:", r.area())
+	fmt.Println("Perimeter:", r.perim())
 }
