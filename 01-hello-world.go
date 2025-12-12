e27801e244b5b4006325871cf6df88193b7aa911
Pelle Lundgren <pellburt@gmail.com>
2025-12-12 08:22:03 +0100

    docs(01): add documentation to hello world example

--- 01-hello-world.go
+++ 01-hello-world.go
@@ -1,7 +1,16 @@
+// Our first program will print the classic "hello world"
+// message. Here's the full source code.
+
+// All Go files start with a package declaration.
+// main package is the entry point of the program.
 package main
 
+// import packages we need.
 import "fmt"
 
+// main function is the entry point of the program.
+// It's the first function that is executed when the program runs.
 func main() {
+	// Println is a function that prints a string to the console.
 	fmt.Println("Hello world!")
 }
