ec0ad56be4da83a65a8f02b9dabeda41011f52be
Pelle Lundgren <pellburt@gmail.com>
2025-12-16 06:14:14 +0100

    feat(02): repeat 02

---
+++ repetition/02.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World! Repetition 2.")
+	fmt.Println("fmt stands for the Format package." + "fmt implements formatted I/O with functions.")
+	fmt.Println("1+1 =", 1+1)
+}
