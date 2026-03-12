0419befe4ecd0abd7ffa354849b7f9a8e359e724
Pelle Lundgren <pellburt@gmail.com>
2026-03-12 06:52:35 +0100

    feat(01): repeat 01

--- repetition/01.go
+++ repetition/01.go
@@ -1,7 +1,10 @@
+// 1. Declare a main package (a package is a way to group functions, and it's made up of all the files in the same directory).
 package main
 
+// 2. Import the popular fmt package, which contains functions for formatting text, including printing to the console. This package is one of the standard library packages you got when you installed Go.
 import "fmt"
 
+// 3. Implement a main function to print a message to the console. A main function executes by default when you run the main package. The Go compiler need to know where to start.
 func main() {
-	fmt.Println("Hello, World! Repetition 1.")
+	fmt.Println("Hello world! Repetition 1")
 }
