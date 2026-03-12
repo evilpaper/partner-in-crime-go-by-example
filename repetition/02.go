5d8b242837f176759ef20ea56237423f376b6d42
Pelle Lundgren <pellburt@gmail.com>
2026-03-12 07:03:10 +0100

    feat(02): repeat 02

--- repetition/02.go
+++ repetition/02.go
@@ -3,7 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World! Repetition 2.")
-	fmt.Println("fmt stands for the Format package." + "fmt implements formatted I/O with functions.")
+
+	// Ok the main setup is done. What should we do here? W e talk values right?
+	// Yes! Go has values. The regular values like strings, integers, floats, booleans, and more.
+	// Let's print some of them and see what they are.
+
+	// Strings. Can be added together with the + operator.
+	fmt.Println("go" + "lang")
+	// Integers. Whole numbers.
 	fmt.Println("1+1 =", 1+1)
+	// Floats. Decimal numbers.
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	// Booleans. True or false. With normal boolean operators you expect.
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
