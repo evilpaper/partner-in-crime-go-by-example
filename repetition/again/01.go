6e7685e8be7a893fff53bfdf79121f5618676d96
Pelle Lundgren <pellburt@gmail.com>
2026-03-13 08:02:32 +0100

    feat(01): repeat 01, again

---
+++ repetition/again/01.go
@@ -0,0 +1,15 @@
+// Let's do this!
+
+package main
+// We need it!
+
+import "fmt"
+// fmt stands for the Format package. It contains function to print to the console. We need it too.
+
+func main() {
+	// We also need this one. The complier look for func main() to start the program.
+	fmt.Println("Hello world! Repetition 1. Again.")
+	// Finally, now we can print. 
+}
+
+// Ok done! Now go runt it from the command line with go run <path to the file>
