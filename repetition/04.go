044cf3585c6d79e5b0a31e5b80d5ebe4b34f5e7b
Pelle Lundgren <pellburt@gmail.com>
2026-03-16 06:43:56 +0100

    feat(04): repeat 04

---
+++ repetition/04.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Ok, lets do this. We talk about constants.
+	// Go support constants. Constants are like variables, but they cannot be changed.
+	
+	// Constants are declared using the const keyword.
+	const s string = "I am a constant"
+
+	// Constants can be string, boolean, and numeric values.
+	const n = 500000000
+
+	fmt.Println(s)
+	fmt.Println(n)
+}
