84370ae27ffbe38649c40d2ca64a8542eb1a5c00
Pelle Lundgren <pellburt@gmail.com>
2026-03-18 07:00:35 +0100

    feat(07): repeat 07

---
+++ repetition/06.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	// Ok, lets do this. We talk about if else statements.
+
+	// Branching with if and else in Go is straightforward.
+	
+	// Here is a basic example.
+	
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+}
