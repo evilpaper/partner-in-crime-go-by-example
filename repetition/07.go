bc9c984e02979095fd69803d8a635a7136f99b18
Pelle Lundgren <pellburt@gmail.com>
2026-03-19 06:35:49 +0100

    feat(07): use comma to separate multiple cases, like this and that

--- repetition/07.go
+++ repetition/07.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
 	// Ok, lets do this. We talk about switch statements.
 
+	// A basic switch statement
 	var i = 2
 	fmt.Print("Write ", i, " as ")
 	switch i {
@@ -15,5 +19,14 @@ func main() {
 		fmt.Println("two")
 	case 3:
 		fmt.Println("three")
+	}
+
+	// We can use comma to separate multiple cases.
+	var weekday = time.Now().Weekday()
+	switch weekday {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
 	}
 }
