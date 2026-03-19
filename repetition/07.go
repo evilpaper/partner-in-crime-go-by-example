d6cda746ac6dc3ca6e98762b6992e52b1f368656
Pelle Lundgren <pellburt@gmail.com>
2026-03-19 06:33:31 +0100

    feat(07): repeat basic switch statement

--- repetition/07.go
+++ repetition/07.go
@@ -5,4 +5,15 @@ import "fmt"
 func main() {
 
 	// Ok, lets do this. We talk about switch statements.
+
+	var i = 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
 }
