63bfa0d9f3a11b7d738995921d451a96f937fb10
Pelle Lundgren <pellburt@gmail.com>
2026-03-22 07:02:16 +0100

    docs(19): break down steps to create a struct

--- repetition/again/19.go
+++ repetition/again/19.go
@@ -4,7 +4,11 @@ import "fmt"
 
 // 1. Structs are collections of fields.
 
-// 2. We can define a struct by using the type keyword and then the struct name and then the fields.
+// 2. We can define a struct by using...
+// 2.1 the type keyword then, 
+// 2.2 the name of the struct then,
+// 2.3 the struct keyword and then,
+// 2.4 the fields.
 
 type person struct {
 	name string
