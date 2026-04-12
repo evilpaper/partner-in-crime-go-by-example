4ab3cfc5dc0836b5f31d3fe17894abd3e0a75183
Pelle Lundgren <pellburt@gmail.com>
2026-04-12 21:51:58 +0200

    feat(08): repeat 08 Arrays

---
+++ repetition/08.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Ok, lets do this. We talk about arrays. Arrays are fixed length sequences of elements of the same type.
+	// Array in Go are not the same as arrays in other languages.
+
+	var a = [5]int{1, 2, 3, 4, 5}
+	fmt.Println("a: ", a)
+	
+}
