91c1070671dec1edfca1ab24ef98850d7d7fe1c6
Pelle Lundgren <pellburt@gmail.com>
2026-04-13 22:18:38 +0200

    docs: flesh out descroption of arrays and slices

--- repetition/08.go
+++ repetition/08.go
@@ -4,10 +4,18 @@ import "fmt"
 
 func main() {
 
-	// Ok, lets do this. We talk about arrays. Arrays are fixed length sequences of elements of the same type.
+	// Ok, lets do this. We talk about arrays. 
+
+	// Arrays are fixed length sequences of elements of the same type.
+
 	// Array in Go are not the same as arrays in other languages.
 
+	// Length is a part of their type and hence they cannot be resized. Weird, I know.
+
+	// Here is a basic example.
 	var a = [5]int{1, 2, 3, 4, 5}
 	fmt.Println("a: ", a)
+
+	// There isn’t a push method because arrays are of fixed lengths in Go
 	
 }
--- repetition/09.go
+++ repetition/09.go
@@ -9,7 +9,8 @@ func main() {
 	// Slices are like arrays but more flexible.
 
 	// Unlike arrays, slices are typed only by the elements they contain (not the number of elements). 
-	// An uninitialized slice equals to nil and has length 0.
+
+	// Here is an uninitialized slice. It equals to nil and has length 0.
 	var s []string
 	fmt.Println("uninit slice:", s, s == nil, len(s) == 0)
 }
